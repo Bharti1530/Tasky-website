674dbad26ebc46d6d04a5c98e2e845906dbaa01e
Bharti1530 <146191324+Bharti1530@users.noreply.github.com>
2023-10-01 09:10:31 +0530

    Add files via upload

---
+++ Tasky Website/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('addProjectButton').addEventListener('click', function () {
+    // Replace 'new-page.html' with the URL of the new page you want to open.
+    window.location.href = 'new-page.html';
+});
